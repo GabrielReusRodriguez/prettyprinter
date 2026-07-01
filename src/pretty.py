b11725735ed2b8adcbd52c11f144e0514d241061
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2026-07-01 19:49:11 +0200

    fix: correccion de bugs en pretty.py
    - Reemplazado bare  por  (capturaba SystemExit/KeyboardInterrupt)
    - Corregido mensaje de error en format_json que mostraba input file en vez de output file
    - Agregados sys.exit(1) en todos los errores I/O para no retornar exit code 0
    - Movido print de debug detras del flag --verbose
    - Eliminado nargs=1 (devolvia listas) en favor del default que devuelve str directamente
    - Eliminadas strings sueltas (docstrings colgantes) reemplazadas por comentarios
    - Agregado import sys

--- src/pretty.py
+++ src/pretty.py
@@ -1,97 +1,88 @@
 #!/bin/env python3
 
 """
-    Programa que le da formato (pretty print ) a ficheros  json y xml.
-    AUTHOR = Gabriel Reus Rodriguez
+Programa que le da formato (pretty print) a ficheros json y xml.
+AUTHOR = Gabriel Reus Rodriguez
 """
 
 import argparse
 import xml.dom.minidom
 import os
 import json
+import sys
 
-PROG_NAME= 'pretty'
-PROG_DESCRIPTION = """Programa que formatea ficheros xml o json.
-"""
+PROG_NAME = "pretty"
+PROG_DESCRIPTION = """Programa que formatea ficheros xml o json."""
 
-"""Auxiliar classes"""
+
+# Auxiliar classes
 class ArgException(Exception):
     pass
 
-"""Functions"""
+
+# Functions
 def treat_args():
-    # Obtengo la instancia de argumentparser.
-    parser = argparse.ArgumentParser(   prog= PROG_NAME,
-                                        description= PROG_DESCRIPTION
-                                    )
+    parser = argparse.ArgumentParser(prog=PROG_NAME, description=PROG_DESCRIPTION)
 
-    parser.add_argument('-x', '--xml', nargs=1, type = str, required= False, help =' XML file')
-    parser.add_argument('-j', '--json', nargs=1, type = str, required= False, help ='JSON file')
-    # Para argumentos que no tienen valor, uso un action.store_true
-    #parser.add_argument('-a', '--auto', action= 'store_true', help ='Autodetect file')
-    parser.add_argument('-a', '--auto', nargs=1, type = str, required= False, help ='Autodetect file' )
-    parser.add_argument('-o', '--output', nargs=1, type = str, required= True, help ='Output file')
-    parser.add_argument('-v', '--verbose', action= 'store_true', help ='verbose')
-    
+    parser.add_argument("-x", "--xml", type=str, required=False, help="XML file")
+    parser.add_argument("-j", "--json", type=str, required=False, help="JSON file")
+    parser.add_argument(
+        "-a", "--auto", type=str, required=False, help="Autodetect file"
+    )
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output file")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
+
     args = parser.parse_args()
-    return (parser, args)
+    return parser, args
 
 
-def format_xml(args:argparse.Namespace):
-    # Creo la instancia del dom.
+def format_xml(args: argparse.Namespace):
     dom = None
-    # La parseo
     try:
-        dom = xml.dom.minidom.parse(args.xml[0])
-    except Exception as e: 
-        print(f'\tError al leer el fichero {args.xml[0]}: {e}')
-        return
-    # Hago el pretty printer
+        dom = xml.dom.minidom.parse(args.xml)
+    except Exception as e:
+        print(f"\tError al leer el fichero {args.xml}: {e}")
+        sys.exit(1)
     pretty_formated = dom.toprettyxml()
-    # Escribo los resultados.
     try:
-        with open(file = args.output[0], mode= 'w') as f:
+        with open(file=args.output, mode="w") as f:
             f.write(pretty_formated)
-    except IOError as e:
-        print(f'\tError al escribir el fichero {args.output[0]}: {e}')
-        return
-    except:
-        print(f'\tError al escribir el fichero {args.output[0]}')
-        return
+    except Exception as e:
+        print(f"\tError al escribir el fichero {args.output}: {e}")
+        sys.exit(1)
 
 
-def format_json(args:argparse.Namespace):
+def format_json(args: argparse.Namespace):
     data = None
-    # Leo el fichero
-    print(f'Voy a leer: {args.json[0]}')
+    if args.verbose:
+        print(f"Voy a leer: {args.json}")
     try:
-        with open(file = args.json[0], mode = 'r') as f:
+        with open(file=args.json, mode="r") as f:
             data = json.load(f)
     except Exception as e:
-        print(f'\tError al leer el fichero {args.json[0]} err: {e}')
-        return
-    # Le doy formato y escribo el fichero
+        print(f"\tError al leer el fichero {args.json} err: {e}")
+        sys.exit(1)
     try:
-        with open(file = args.output[0], mode = 'w') as f:
-            json.dump(data, fp = f, sort_keys = True, indent = 4)
-    except IOError as e:
-        print(f'\tError al escribir el fichero {args.json[0]}: {e}')
-        return
+        with open(file=args.output, mode="w") as f:
+            json.dump(data, fp=f, sort_keys=True, indent=4)
+    except Exception as e:
+        print(f"\tError al escribir el fichero {args.output}: {e}")
+        sys.exit(1)
 
 
-def format_auto(args:argparse.Namespace):
-    # Primero obtengo la extension del fichero.
-    filename, file_extension = os.path.splitext(args.auto[0])
-    if file_extension.lower() == '.xml':
-        args.xml = [args.auto[0]]
+def format_auto(args: argparse.Namespace):
+    filename, file_extension = os.path.splitext(args.auto)
+    if file_extension.lower() == ".xml":
+        args.xml = args.auto
         format_xml(args)
         return
-    if file_extension.lower() == '.json':
-        args.json = [args.auto[0]]
+    if file_extension.lower() == ".json":
+        args.json = args.auto
         format_json(args)
         return
-    print(f'ERROR: extension NO identificada {args.auto[0]}')
-    return
+    print(f"ERROR: extension NO identificada {args.auto}")
+    sys.exit(1)
+
 
 def pretty(args: argparse.Namespace):
     if args.xml is not None:
@@ -105,12 +96,12 @@ def pretty(args: argparse.Namespace):
         return
     raise ArgException()
 
-"""main"""
-if __name__ == '__main__':
+
+# main
+if __name__ == "__main__":
     parser, args = treat_args()
     try:
         pretty(args)
-    except ArgException as e:
-        print(f'\tError parseando los argumentos')
+    except ArgException:
+        print("\tError parseando los argumentos")
         parser.print_help()
-
